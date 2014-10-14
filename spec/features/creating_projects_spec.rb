b7e95f29b7d229773565699775405c9f8cd96952
NandoGama <nandogama63@gmail.com>
2014-10-14 16:00:46 +0100

    You can now update a Project

--- spec/features/creating_projects_spec.rb
+++ spec/features/creating_projects_spec.rb
@@ -8,19 +8,18 @@ RSpec.configure do |c|
           click_link 'New Project'
         end
         scenario "can create a project" do
-          fill_in 'Name', with: 'Lodge'
-          fill_in 'Description', with: 'A web app for Lodge'
-          click_button 'Create Project'
-          expect(page).to have_content('Project has been created.')
-          project = Project.where(name: "TextMate 2").first
-          expect(page.current_url).to eql(project_url(project))
-          title = "Lodge - Projects - turtulias"
-          expect(page).to have_title(title)
+            fill_in 'Name', with: 'TextMate 2'
+            fill_in 'Description', with: 'A text-editor for OS X'
+            click_button 'Create Project'
+            expect(page).to have_content('Project has been created.')
+            project = Project.where(name: "TextMate 2").first
+            expect(page.current_url).to eql(project_url(project))
+            title = "TextMate 2 - Projects - turtulias"
+            expect(page).to have_title(title)
+          end
+          scenario "can not create a project without a name" do
+            click_button 'Create Project'
+            expect(page).to have_content("Project has not been created.")
+            expect(page).to have_content("Name can't be blank")
+          end
         end
-        scenario "can not create a project without a name" do
-          click_button 'Create Project'
-          expect(page).to have_content("Project has not been created.")
-          expect(page).to have_content("Name can't be blank")
-        end
-      end
-
