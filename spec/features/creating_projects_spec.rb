f35f7df563e146a81cb9668694e6209210d213b4
NandoGama <nandogama63@gmail.com>
2014-10-14 14:20:16 +0100

    Add validation to ensure names are

--- spec/features/creating_projects_spec.rb
+++ spec/features/creating_projects_spec.rb
@@ -2,20 +2,25 @@ require 'spec_helper'
 RSpec.configure do |c|
         c.expose_current_running_example_as :example
       end
-feature 'Creating Projects' do
-  scenario "can create a project" do
-    visit '/'
-    click_link 'New Project'
-    fill_in 'Name', with: 'Lodge'
-    fill_in 'Description', with: 'A web app for Lodge'
-    click_button 'Create Project'
-    expect(page).to have_content('Project has been created.')
-  
-  project = Project.where(name: "Lodge").first
-  expect(page.current_url).to eql(project_url(project))
-  title = "Lodge - Projects - turtulias"
-  expect(page).to have_title(title)
-  
-  end
-end
+      feature 'Creating Projects' do
+        before do
+      visit '/'
+          click_link 'New Project'
+        end
+        scenario "can create a project" do
+          fill_in 'Name', with: 'Lodge'
+          fill_in 'Description', with: 'A web app for Lodge'
+          click_button 'Create Project'
+          expect(page).to have_content('Project has been created.')
+          project = Project.where(name: "TextMate 2").first
+          expect(page.current_url).to eql(project_url(project))
+          title = "Lodge - Projects - turtulias"
+          expect(page).to have_title(title)
+        end
+        scenario "can not create a project without a name" do
+          click_button 'Create Project'
+          expect(page).to have_content("Project has not been created.")
+          expect(page).to have_content("Name can't be blank")
+        end
+      end
 
